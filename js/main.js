2b816c3f2cd6ff2d6030721dc511782b52e28cef
ord669 <ord24@gmail.com>
2022-12-05 20:02:42 +0700

    renderd gallery not yet modal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
 console.log('Starting up');
+
+
+const gPortfolio = [
+    {
+        id: "sokoban", 
+        name: "Sokoban", 
+        title: "Better push those boxes", 
+        desc: "lorem ipsum lorem ipsum lorem ipsum", 
+        url: "projs/sokoban", 
+        publishedAt: 1448693940000, 
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "sokoban", 
+        name: "Sokoban", 
+        title: "Better push those boxes", 
+        desc: "lorem ipsum lorem ipsum lorem ipsum", 
+        url: "projs/sokoban", 
+        publishedAt: 1448693940000, 
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "sokoban", 
+        name: "Sokoban", 
+        title: "Better push those boxes", 
+        desc: "lorem ipsum lorem ipsum lorem ipsum", 
+        url: "projs/sokoban", 
+        publishedAt: 1448693940000, 
+        labels: ["Matrixes", "keyboard events"],
+    }
+]
+
+function getgPortfolio(){
+    return gPortfolio
+}
+
+
+
+
+
+const gModal =[{
+    
+}
+]
+
+function getgModal(){
+    return gModal
+}
